392722ca040c98d65823d60fb0b5984588c9eb63
Andrew-bot1 <aahayes27@amarillocollege.com>
2025-05-04 18:11:02 -0500

    Update project11-01.js

--- project111_Hayes/project11-01.js
+++ project111_Hayes/project11-01.js
@@ -16,11 +16,36 @@ let dateBox = document.getElementById("dateBox");
 dateBox.onchange = function() {   
       //3a set dateStr to value of datebox
       let dateStr=dateBox.value;
+      console.log(dateStr);
       //3b fetch url
-      fetch("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date"=dateStr)
-      .then(response.json)
-
+      fetch("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date="+dateStr)
+      //3c add then method to get to parse json string
+      .then(response=>response.json())
+      //3d run showPicture method with json object
+      .then(json=>showPicture(json))
+      //add catch method to display promise
+      .catch(error=>console.log(error));
 }
+
+//4 create function to show picture
+function showPicture(json) {
+      //4a see if json type is video
+      if (json.media_type==="video") {
+            //change html of image box
+            imageBox.innerHTML="<iframe src ='"+json.url+"'></iframe><h1>"+json.title+"</h1><p>"+json.explanation+"</p>";
+      }
+      //see if type is image
+      else if(json.media_type==="image"){
+            //change html
+            imageBox.innerHTML="<img src ='"+json.url+"'/><h1>"+json.title+"</h1><p>"+json.explanation+"</p>";
+      }
+      else {
+            //show image is not available
+            imageBox.innerHTML="Image not Available";
+      }
+}
+
+
 
 
 
